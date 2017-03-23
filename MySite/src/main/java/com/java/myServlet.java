373cd5854460c25b848a89e353beb2f1b6e4bcb6
hyunwoo <skyghkd@naver.com>
2017-03-23 14:01:22 +0900

    ㅋㅁ

--- MySite/src/main/java/com/java/myServlet.java
+++ MySite/src/main/java/com/java/myServlet.java
@@ -6,12 +6,13 @@ import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+@WebServlet("/Sv")
 public class myServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
